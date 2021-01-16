3ab943309a991a10902a133825d4cc9a6aa124e2
ryunosuke-noyama <ryu1998.08.21@icloud.com>
2021-01-16 15:43:10 +0900

    チャット画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,7 +15,7 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_id: [])
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
 
